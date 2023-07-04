12aeca1ae2366da4918d16a1fde1fa699b304501
pedronunomacedo <pedronunomacedo@hotmail.com>
2023-07-04 20:54:54 +0100

    Removed folder cmake-build-debug

--- Fichas/DA_TP_Classes/cmake-build-debug/CMakeFiles/TP3a.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TP3a.
-# This may be replaced when dependencies are built.
--- Fichas/DA_TP_Classes/cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/pedromacedo/Desktop/University/Year2/Semester2/DA-Year2Semester2/DA_TP_Classes/lib/googletest-master
-# Build directory: /home/pedromacedo/Desktop/University/Year2/Semester2/DA-Year2Semester2/DA_TP_Classes/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
